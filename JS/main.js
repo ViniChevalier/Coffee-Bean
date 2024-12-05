adcf1d9fb168e9a06904b28bf10df7becbaaaf50
DMS <x23157607@student.ncirl.ie>
2024-12-05 20:00:35 +0000

    Debug

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,3 @@
-/*
-	Helios by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
 
 (function ($) {
 
@@ -161,7 +156,6 @@
 		});
 
 	});
-
 	
 
 })(jQuery);
